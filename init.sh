0e70f479a3942028c5f90b7732fc0a5c3009af54
pinarruiz <37040888+pinarruiz@users.noreply.github.com>
2020-11-30 11:53:00 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-yum upgrade -y
-yum install wget nano  -y
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz"
+apt update
+apt upgrade -y
+apt install wget nano curl unzip -y
